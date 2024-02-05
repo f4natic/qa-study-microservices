432ca9ce24025af26c07a90827c62b64770386b2
krutikov <i.krutikov@cft.ru>
2024-02-05 12:14:32 +0700

    Изменил поля и добавил документацию

--- product-service/src/main/java/com/example/Application.java
+++ product-service/src/main/java/com/example/Application.java
@@ -9,7 +9,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        Product product = new Product.Builder().id(null).name(null).price(null).build();
     }
 }
--- product-service/src/main/java/com/example/model/Product.java
+++ product-service/src/main/java/com/example/model/Product.java
@@ -5,26 +5,17 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "products")
 public class Product {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Column(name = "name", unique = true)
     private String name;
-
     @Column(name = "price", scale = 4)
     private Double price;
+    @Column(name = "manufacturer")
+    private String manufacturer;
 
-    public Product() {}
-
-    private Product(Builder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.price = builder.price;
-    }
-
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -32,41 +23,11 @@ public class Product {
         return name;
     }
 
-    public double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
-    }
-
-    public static class Builder {
-        private Long id;
-        private String name;
-        private Double price;
-
-        public Builder id(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder price(Double price) {
-            this.price = price;
-            return this;
-        }
-
-        public Product build() {
-            return new Product(this);
-        }
+    public String getManufacturer() {
+        return manufacturer;
     }
 }
